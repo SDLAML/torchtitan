e9e661de4862a338a6331f3a60b6efe93362f389
rakkit <rakkit@users.noreply.github.com>
2025-05-26 21:33:57 +0200

    Allow configuring Scion backend

--- torchtitan/components/optimizer.py
+++ torchtitan/components/optimizer.py
@@ -613,6 +613,7 @@ def build_optimizers(
         }
     elif is_scion:
         backend_steps = optimizer_config.backend_steps
+        zeropower_backend_algorithm = optimizer_config.zeropower_backend
         momentum = optimizer_config.momentum
         nesterov = optimizer_config.nesterov
         is_light = optimizer_config.is_light
@@ -626,7 +627,7 @@ def build_optimizers(
             "nesterov": nesterov,
             "eps": eps,
             "norm_factor": "spectral",
-            "backend": "newtonschulz5",
+            "backend": zeropower_backend_algorithm,
             "backend_steps": backend_steps,
         }
     else:
