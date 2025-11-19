cc8695e5e326000f85b42763737fe7994c53348a
rakkit <26144573+rakkit@users.noreply.github.com>
2025-11-19 11:36:30 +0100

    fix train.py and args after rebase. and refactor args

--- torchtitan/hf_datasets/text_datasets.py
+++ torchtitan/hf_datasets/text_datasets.py
@@ -504,7 +504,7 @@ def _replace_none_with_literal(xs: list[str] | None) -> list[str | None] | None:
     return xs
 
 
-def build_hf_dataloader(
+def build_text_dataloader(
     dp_world_size: int,
     dp_rank: int,
     tokenizer: BaseTokenizer,
@@ -603,7 +603,7 @@ def build_hf_dataloader(
         )
 
     if job_config.training.dataset_seed is None:
-        job_config.training.dataset_seed = job_config.training.seed
+        job_config.training.dataset_seed = job_config.debug.seed
 
     if job_config.training.dataset_shuffle_buffer_size:
         hf_ds = WindowShuffledDataset(
@@ -628,7 +628,7 @@ def build_hf_dataloader(
     )
 
 
-def build_hf_validation_dataloader(
+def build_text_validation_dataloader(
     dp_world_size: int,
     dp_rank: int,
     tokenizer: BaseTokenizer,
