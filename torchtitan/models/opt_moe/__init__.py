f449a8e8766b1ff85fa9a6750fbab4c893aa5a21
rakkit <26144573+rakkit@users.noreply.github.com>
2026-04-07 16:03:56 +0200

    add mis-8b-proxy

--- torchtitan/models/opt_moe/__init__.py
+++ torchtitan/models/opt_moe/__init__.py
@@ -249,7 +249,7 @@ moe_opt_moe_configs = {
     #
     #
     #
-    "macaron-8b": OPTMoEModel.Config(
+    "mis-8b": OPTMoEModel.Config(
         n_layers=24,
         dim=2048,
         swa_pattern="SSSF" * 6,
@@ -272,6 +272,45 @@ moe_opt_moe_configs = {
             attention=GatedNormSWAttention.Config(
                 n_heads=32,
                 n_kv_heads=4,
+                head_dim=128,
+                qk_norm=True,
+                norm_everywhere=True,
+                norm_eps=1e-30,
+                sliding_window_size=128,
+                attn_mask_type="block_causal",
+                attn_backend="flex",
+            ),
+        ),
+        rope=RoPE.Config(
+            dim=128,
+            max_seq_len=4096,
+            theta=10000.0,
+            backend="cos_sin",
+        ),
+    ),
+    "mis-8b-proxy": OPTMoEModel.Config(
+        n_layers=24,
+        dim=512,
+        swa_pattern="SSSF" * 6,
+        rope_pattern="RRRN" * 6,
+        layer=OPTMoETransformerBlock.Config(
+            n_dense_layers=0,
+            feed_forward=FeedForward.Config(
+                hidden_dim=1280,
+                norm_everywhere=True,
+                norm_eps=1e-30,
+            ),
+            moe=MoE.Config(
+                hidden_dim=160,
+                num_experts=64,
+                num_shared_experts=1,
+                top_k=8,
+                norm_everywhere=True,
+                scaling_factor=2.8232,
+            ),
+            attention=GatedNormSWAttention.Config(
+                n_heads=8,
+                n_kv_heads=1,
                 head_dim=128,
                 qk_norm=True,
                 norm_everywhere=True,
